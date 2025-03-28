0d407cac075345e8ebb5c0760d9e57389157fc36
bakarjani476 <168990674+bakarjani476@users.noreply.github.com>
2025-03-28 04:29:18 -0700

    Add files via upload

---
+++ Variables and data types.py
@@ -0,0 +1,50 @@
+print('hello world')
+
+# variables
+# variables act as containers for data
+# you can assign a value to a variable using the assignment operator (=)
+# They allow us to manipulate and work with data efficiently
+# They have dynamic nature as they can hold all types of data
+# Variables are case sensitive, so 'a' and 'A' are treated as different variables
+# Variables can be reassigned with new values
+
+# There are three standardly used methods in naming a variable
+
+# 1. Snake case: In this method all the characters of variable name are in 
+# lower case and words are separated by underscore
+# example: hello_world or user_name
+
+# 2. Camel case: In this method the characters fo first word will be lower
+# case and the first character of the second word will be upper case
+# and there will be no any space,hyphen or underscore between them
+# example: helloWorld or userName
+
+# 3.Pascal case: In this method the first letter of all the words in the 
+# variable name would be capital and remaining letters will be in lower 
+# case and there will be no any space,hyphen or underscore between them
+# example: HelloWorld or UserName
+
+# It is recommended that if in a program we use a case suppose pascal case
+# then we should use it throughout the program for all the variable names
+# so that it is easy to understand and read the program
+
+fruit_basket = "apple","banana","orange"
+print(fruit_basket)
+
+numbers = 1,2,3,4,5
+print(numbers)
+
+num = (1,2,4,6,8)
+print(num)
+
+numb = [1,3,5,7,9]
+print(numb)
+
+number2 = "1,2,3,4,5"   # It is a string
+print(number2)
+
+number3 = '1','2','3','4','5'  # it is a tuple data type
+print(number3)
+
+print(type(number3))
+
